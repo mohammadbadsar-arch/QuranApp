7b11c149252f0f0e6ef7998ae6973cd48ee26ad1
mohammadbadsar-arch <mohammadbadsar@gmail.com>
2026-07-06 16:08:31 +0330

    Update ApiService.kt

--- app/src/main/java/com/example/quranapp/ApiService.kt
+++ app/src/main/java/com/example/quranapp/ApiService.kt
@@ -1,15 +1,17 @@
+package com.example.quranapp
+
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiService {
+
     @FormUrlEncoded
     @POST("register.php")
     fun registerUser(
         @Field("username") username: String,
-        @Field("password") password: String,
-        @Field("role") role: String
+        @Field("password") password: String
     ): Call<AuthResponse>
 
     @FormUrlEncoded

07f02a62a5b4ad7fc5047bf6e271114d77ded7de
mohammadbadsar-arch <mohammadbadsar@gmail.com>
2026-07-06 16:03:48 +0330

    Update RetrofitClient.kt

--- app/src/main/java/com/example/quranapp/RetrofitClient.kt
+++ app/src/main/java/com/example/quranapp/RetrofitClient.kt
@@ -1,14 +1,16 @@
+package com.example.quranapp
+
 import retrofit2.Retrofit
-import retrofit2.converter.gson:GsonConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // آدرس مخصوص شبیه‌ساز اندروید برای دسترسی به لوکال‌هارست سیستم
-    private const val BASE_URL = "http://10.0.2.2/quran_api/" 
+    // آدرس IP برای اتصال شبیه‌ساز اندروید استودیو به لوکال‌هاست سیستم شما
+    private const val BASE_URL = "http://10.0.2.2/quran_api/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()) // الان این کلاس به درستی شناخته می‌شود
             .build()
 
         retrofit.create(ApiService::class.java)

6fa89dcfccf46014d2dcf167041fd99671588265
mohammadbadsar-arch <mohammadbadsar@gmail.com>
2026-07-09 17:30:05 +0330

    Update Models.kt

--- app/src/main/java/com/example/quranapp/Models.kt
+++ app/src/main/java/com/example/quranapp/Models.kt
@@ -6,3 +6,20 @@ data class AuthResponse(
     val user_id: Int? = null,
     val role: String? = null
 )
+
+// مدل‌های جدید برای دریافت وضعیت پیشرفت از سرور
+data class ProgressResponse(
+    val error: Boolean,
+    val message: String
+)
+
+data class ProgressItem(
+    val verse_index: String,
+    val status: String
+)
+
+data class GetProgressResponse(
+    val error: Boolean,
+    val message: String?,
+    val progress: List<ProgressItem>?
+)
